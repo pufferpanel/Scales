eca0741f577b1709cb6573be111d0eeed38eb0bc
Dane Everitt <DaneEveritt@users.noreply.github.com>
2015-03-16 22:39:43 -0400

    Add support for adding new users to the system
    Untested; uses SFTP to manage user accounts with no shell access.
    
    Moved creation code to a script to allow flexibility and multi-system
    support.

---
+++ lib/scripts/create_user.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# ==============================
+# Scales User Management Script
+# Written for Ubuntu Sysems
+#
+# ./create_user.sh /home/ username password
+# ==============================
+
+useradd -m -d $1$2 -g scalesuser -p $(openssl passwd -1 $3) $2
+
+chown root:root $1$2
+chmod 755 $1$2
+
+cd $1$2
+mkdir public backups
+chown $2:scalesuser *
