bd0ca56adcf36fd9274a6686f965c12e0744d2eb
Dane Everitt <DaneEveritt@users.noreply.github.com>
2015-04-30 00:04:24 -0400

    Install SteamCMD each time we install a server
    To prevent issues that might arise if a corrupted file is downloaded

--- lib/scripts/srcds_install.sh
+++ lib/scripts/srcds_install.sh
@@ -29,24 +29,14 @@ fi;
 
 shift $((OPTIND-1))
 
-if [ ! -f "/srv/steamcmd/steamcmd.sh" ]; then
-
-	mkdir -p /srv/steamcmd && cd /srv/steamcmd
-	curl -O http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-	tar -xzvf steamcmd_linux.tar.gz && rm -rf steamcmd_linux.tar.gz
-
-fi
-
 cd /home/${username}/public
 
 mkdir steamcmd
 
-cp -R /srv/steamcmd/* /home/${username}/public/steamcmd
-chown -R ${username}:scalesuser *
+curl -O http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+tar -xzvf steamcmd_linux.tar.gz && rm -rf steamcmd_linux.tar.gz
 
-# mkdir -p .steam/sdk32
-# cd .steam/sdk32
-# ln -s /srv/steamcmd/linux32/steamclient.so
+chown -R ${username}:scalesuser *
 
 # SteamCMD is strange about the user who installs it and where it places some files.
 su - ${username} -c "cd public/steamcmd && ./steamcmd.sh +login anonymous +force_install_dir /home/${username}/public +app_update $2 +quit 2>&1"
