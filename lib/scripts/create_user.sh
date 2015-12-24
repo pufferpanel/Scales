1789924e4e1cb7129aed5037b2346bdc9c7fff73
Lord_Ralex <lordralex@gmail.com>
2015-12-23 18:27:13 -0600

    Fix srcds within docker for now

--- lib/scripts/create_user.sh
+++ lib/scripts/create_user.sh
@@ -3,8 +3,7 @@
 useradd -m -d $1$2 -s /bin/false -p $(openssl passwd -1 $3) $2
 usermod -G scalesuser $2
 
-chown root:root $1$2
 chmod 755 $1$2
 
 mkdir $1$2/public $1$2/backups
-chown $2:scalesuser *
+chown $2:scalesuser $1$2/*
