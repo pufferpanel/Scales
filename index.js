b6e4f5f5e75471a19598717980796b128c604767
Lord_Ralex <lordralex@gmail.com>
2016-01-16 19:38:21 -0600

    Add custom daemon process management

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ var Rfr = require('rfr');
 var Async = require('async');
 var Proc = require('child_process');
 var Util = require('util');
+var Fs = require('fs-extra');
 var Logger = Rfr('lib/logger.js');
 var Config = Rfr('config.json');
+var Vargs = require('yargs');
+var sleep = require('sleep').sleep;
+
+var cliArgs = Vargs.argv;
 
 Logger.info('+ ========================================== +');
 Logger.info('| Scales logs all information, (inc. errors) |');
@@ -18,44 +23,54 @@ Logger.info('| '.reset + 'Submit bug reports at the following link: '.red + ' |'
 Logger.info('| https://github.com/PufferPanel/Scales      |');
 Logger.info('+ ========================================== +');
 
+var pidFilePath = __dirname + '/scales.pid';
+
 Logger.verbose('Using docker?: ' + ((typeof Config.docker === 'undefined') || (Config.docker) == true));
 
-Proc.exec('find ./lib/scripts -name "*.sh" -exec chmod +x {} \\;', function (err, stdout, stderr) {
+Proc.execSync('find ./lib/scripts -name "*.sh" -exec chmod +x {} \\;', function (err, stdout, stderr) {
 
     if (err) {
         Logger.error('An error occurred while attempting to correct script permissions on boot.', stderr);
         process.exit(1);
-    } else {
-        Logger.verbose('All scripts in /lib/scripts successfully had their permissions updated.');
-        Rfr('lib/interfaces/restify.js');
-        Rfr('lib/interfaces/socket.js');
-
-        process.on('SIGINT', function () {
-
-            var servers = Rfr('lib/initalize.js').servers;
-
-            Logger.warn('Detected shutdown! Stopping all running server containers.');
-            Async.forEachOf(servers, function (value, key, next) {
-
-                if (typeof servers[key] !== 'undefined') {
-                    try {
-                        servers[key]._stop(function () {
-                        });
-                    } catch (err) {
-                        Logger.error(Util.format('Unexpected error shutting down server %s', key), err);
-                    }
-                }
-
-                return next();
-            }, function (err) {
-
-                if(err) {
-                    Logger.error('An error was detected while shutting down', err);
-                }
-                Logger.warn('All running server containers stopped successfully.');
-                Logger.shutdown();
-                process.exit(0);
-            });
-        });
     }
+});
+
+Logger.verbose('All scripts in /lib/scripts successfully had their permissions updated.');
+Rfr('lib/interfaces/restify.js');
+Rfr('lib/interfaces/socket.js');
+
+Logger.info('Scales has completed started');
+if (!cliArgs.nodaemon) {
+    require('daemon')();
+}
+
+Fs.writeFileSync(pidFilePath, process.pid);
+process.on('SIGINT', function () {
+
+    var servers = Rfr('lib/initalize.js').servers;
+
+    Logger.warn('Detected shutdown! Stopping all running server containers.');
+    Async.forEachOf(servers, function (value, key, next) {
+
+        if (typeof servers[key] !== 'undefined') {
+            try {
+                servers[key]._stop(function () {
+                });
+            } catch (err) {
+                Logger.error(Util.format('Unexpected error shutting down server %s', key), err);
+            }
+        }
+
+        return next();
+    }, function (err) {
+
+        if (err) {
+            Logger.error('An error was detected while shutting down', err);
+        }
+        Logger.warn('All running server containers stopped successfully.');
+        Logger.shutdown();
+        sleep(2);
+        Fs.unlinkSync(pidFilePath);
+        process.exit(0);
+    });
 });
