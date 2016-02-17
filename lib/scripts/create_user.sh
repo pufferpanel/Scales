de87d6b10f3308f4655b73df42ca08aaec65b176
Joshua Taylor <lordralex@gmail.com>
2016-02-16 23:22:07 -0600

    Permissions are so annoying

--- lib/scripts/create_user.sh
+++ lib/scripts/create_user.sh
@@ -4,7 +4,7 @@ useradd -m -d $1/$2 -s /bin/false -p $(openssl passwd -1 $3) $2
 usermod -G scalesuser $2
 
 chown root:$2 $1/$2
-chmod 770 $1/$2
+chmod 755 $1/$2
 
 mkdir $1/$2/public $1/$2/backups
 chown $2:$2 $1/$2/*
